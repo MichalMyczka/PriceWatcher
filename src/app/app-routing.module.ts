eb3ca2e5e660cacbb09843d086f04b4d08298186
Michal Myczka <michalmorgoth@gmail.com>
2021-01-20 10:20:56 +0100

    early features

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CurrenciesComponent } from './currencies/currencies.component';
+import { CryptocurrenciesComponent} from './cryptocurrencies/cryptocurrencies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'currencies', component: CurrenciesComponent},
+  {path: 'cryptocurrencies', component: CryptocurrenciesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { CurrenciesComponent } from './currencies/currencies.component';
+import { CryptocurrenciesComponent } from './cryptocurrencies/cryptocurrencies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    CurrenciesComponent,
+    CryptocurrenciesComponent
   ],
   imports: [
     BrowserModule,
