90d9e5e3bdd436987a364e7973fd08f059f28426
maciek <mcyran1996@gmail.com>
2021-02-25 10:09:12 +0100

    fake commit

--- src/app/cryptocurrencies/cryptocurrencies.component.ts
+++ src/app/cryptocurrencies/cryptocurrencies.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
 import {CryptocurrenciesService} from '../services/cryptocurrencies.service';
 import {Cryptocurrency} from '../models/cryptocurrency.model';
 import {FirebaseService} from '../services/firebase.service';
 import {FirebaseDBService} from '../services/firebase-db.service';
+import {CryptocurrencyRates} from '../models/cryptocurrency-rates.model';
+import {CryptocurrencyBase} from '../models/cryptocurrencybase.model';
 
 @Component({
   selector: 'app-cryptocurrencies',
@@ -13,8 +15,11 @@ export class CryptocurrenciesComponent implements OnInit {
 
   public cryptoCurrencyList: Cryptocurrency;
   public cryptoCurrencyBase: string;
+  public searchRates: CryptocurrencyBase[];
 
-  constructor(private cryptocurrency: CryptocurrenciesService, public firebaseService: FirebaseService, public firebaseDB: FirebaseDBService) { }
+  constructor(private cryptocurrency: CryptocurrenciesService,
+              public firebaseService: FirebaseService,
+              public firebaseDB: FirebaseDBService) { }
 
   ngOnInit(): void {
     this.getCryptoCurrencies();
@@ -24,6 +29,8 @@ export class CryptocurrenciesComponent implements OnInit {
     this.cryptocurrency.getCrypto(this.cryptoCurrencyBase)
       .subscribe(data => {
         this.cryptoCurrencyList = data;
+        this.searchRates = this.cryptoCurrencyList.rates;
+        console.log(this.cryptoCurrencyList.rates);
       });
   }
 
